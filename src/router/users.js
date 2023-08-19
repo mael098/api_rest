76fc48b62f4e75f1a760231e1ee6958b75062f36
mael098 <juanvillegasmorales@outlook.com>
2023-08-19 09:57:55 -0600

    commit

--- src/router/users.js
+++ src/router/users.js
@@ -1,28 +1,27 @@
 const { Router } = require("express")
-const router = Router()
 const users = require("./user.json")
+const router = Router()
 
 // const fetch = ('node-fetch')
-// router.get('/', async (req, res) => {
-//     const response = await fetch("https://jsonplaceholder.typicode.com/users")
-//     const users = await response.json()
-//     console.log(users);
-//     res.json(users)
-// })
-
-router.get("/", (req, res) => {
-  res.json(users)
-})
-
-router.get("/", (req, res) => {
-  const { user, pass } = req.body
-  if (user && pass) {
-    const id = (users.length = 1)
-    const newUsers = { ...req.body, id }
-    users.push(newUsers)
+router.get('/', async (req, res) => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const users = await response.json()
+    console.log(users);
     res.json(users)
-  }
 })
+
+router.post('/',(req,res) => {
+    const {name,username,email} = req.body
+    if(name && username && email) {
+        const id = users.length + 1
+        const newUser = {...req.body, id}
+        users.push(newUser)
+        res.json(users)
+    }else{
+        res.status(500).json({error:'There was an error'})
+    }
+})
+
 
 
 
