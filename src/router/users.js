2a72103ad6f2fe728a8b9b0d9216aa5b40a3b638
mael098 <juanvillegasmorales@outlook.com>
2023-08-19 16:33:16 -0600

    agruge un post

--- src/router/users.js
+++ src/router/users.js
@@ -1,28 +1,26 @@
-const { Router } = require("express")
+const Router = require("express")
 const users = require("./user.json")
+const _ = require("underscore")
+
 const router = Router()
 
-// const fetch = ('node-fetch')
-router.get('/', async (req, res) => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users")
-    const users = await response.json()
-    console.log(users);
+router.get("/", (req, res) => {
+  res.json(users)
+})
+
+router.post("/", (req, res) => {
+  const { nombre, email, password } = req.body
+  if (nombre && email && password) {
+    const id = users.length + 1
+    const newUser = { ...req.body, id }
+    users.push(newUser)
     res.json(users)
+  } else {
+    res.status(500).json({ error: "There was an error." })
+  }
 })
-
-router.post('/',(req,res) => {
-    const {name,username,email} = req.body
-    if(name && username && email) {
-        const id = users.length + 1
-        const newUser = {...req.body, id}
-        users.push(newUser)
-        res.json(users)
-    }else{
-        res.status(500).json({error:'There was an error'})
-    }
-})
-
 
 
 
 module.exports = router
+
